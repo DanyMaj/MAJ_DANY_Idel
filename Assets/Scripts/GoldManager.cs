121e906e2bdb9221a1d5f62d03890e1c1652a97c
DanyMaj <d.majani@rns.ecolescreatives.com>
2025-12-11 18:01:22 +0100

    V4

--- Assets/Scripts/GoldManager.cs
+++ Assets/Scripts/GoldManager.cs
@@ -7,8 +7,10 @@ using NUnit.Framework.Constraints;
 
 public class GoldManager : MonoBehaviour
 {
-    public int goldAmount;
+    public float goldAmount;
     public int power;
+    public int timeGold;
+    public bool shouldGoldRiseWithTime;
     public int powerPrice;
     public int timePower;
     public int timePrice;
@@ -17,32 +19,34 @@ public class GoldManager : MonoBehaviour
     public TextMeshProUGUI powerPriceText;
     public TextMeshProUGUI timePowerText;
     public TextMeshProUGUI timePowerPriceText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         timePower = 0;
+        timeGold = 25;
         timePrice = 100;
         power = 0;
         powerPrice = 10;
-        StartCoroutine(autoGold()); 
+        StartCoroutine(autoGold());
+        
     }
 
     public void ChangeGold()
     {
         goldAmount += power+1;
-        goldText.text = goldAmount.ToString("00");
-        goldAmount += timePower + 1;
     }
 
 
     public void LaunchCoroutine()
     {
+  
         if (goldAmount >= timePrice)
         {
+            shouldGoldRiseWithTime = true;
             goldAmount -= timePrice;
-            goldText.text = goldAmount.ToString("00");
             timePrice = Mathf.CeilToInt(timePrice * 2.2f);
-
+            timeGold = Mathf.CeilToInt(timeGold * 1.1f);
             timePower += 1;
         }
     }
@@ -52,7 +56,6 @@ public class GoldManager : MonoBehaviour
         if (goldAmount >= powerPrice)
         {
             goldAmount -= powerPrice;
-            goldText.text = goldAmount.ToString("00");
             powerPrice = Mathf.CeilToInt(powerPrice * 2.2f);
             
             power += 1;
@@ -61,26 +64,68 @@ public class GoldManager : MonoBehaviour
 
     public IEnumerator autoGold()
     {
-    
         while (true)
         {
-            goldAmount += timePrice;
-            yield return new WaitForSeconds(10);
+            yield return new WaitForSeconds(5);
+            if(shouldGoldRiseWithTime)
+            {
+                goldAmount += timeGold;
+            }
         }
     }
 
 
+    private string goldTextTemp;
     // Update is called once per frame
     void Update()
     {
-        powerPriceText.text = powerPrice.ToString("00");
-        powerText.text = power.ToString("00");
-        timePowerText.text = timePower.ToString("00"); 
-        timePowerPriceText.text = timePrice.ToString("00");
 
-        if (goldAmount >= 1000)
+
+        if(goldAmount>=1000)
         {
-            goldText.text = (goldAmount / 1000).ToString("00K");
+            goldTextTemp = ((float)goldAmount / 1000f).ToString("0.0K");
         }
+        else
+        {
+            goldTextTemp = goldAmount.ToString("0");
+        }
+
+        if(goldAmount >= 1000000)
+        {
+            goldTextTemp = ((float)goldAmount / 1000000f).ToString("0.0M");
+        }
+
+        if (goldAmount >= 1000000000)
+        {
+            goldTextTemp = ((float)goldAmount / 1000000000f).ToString("0.0Md");
+        }
+
+        if (goldAmount >= 1000000000000)
+        {
+            goldTextTemp = ((float)goldAmount / 1000000000000f).ToString("0.0Bn");
+        }
+
+        if (goldAmount >= 1000000000000000)
+        {
+            goldTextTemp = ((float)goldAmount / 1000000000000000f).ToString("0.0Bd");
+        }
+
+        if (goldAmount >= 1000000000000000000)
+        {
+            goldTextTemp = ((float)goldAmount / 1000000000000000000f).ToString("0.0Tn");
+        }
+
+        //if (goldAmount >= 1000000000000000000000)
+        {
+            //goldTextTemp = ((float)goldAmount / 1000000000000000000000f).ToString("0.0Td");
+        }
+
+        goldText.text = goldTextTemp;
+
+        powerPriceText.text = powerPrice.ToString("0");
+        powerText.text = power.ToString("0");
+        timePowerText.text = timePower.ToString("0");
+        timePowerPriceText.text = timePrice.ToString("0");
     }
+
 }
